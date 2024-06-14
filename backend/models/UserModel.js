879f336f0fadbcb0f79eca0f204a0e276d016c0e
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-06-14 15:37:07 +0530

    implemented email and password validation
     implemented this validation feature using the validator node package. It was implemented in the signup static function of the userSchema

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,3 +1,4 @@
+const validator = require("validator");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const { schema } = require("./workoutModel");
@@ -17,6 +18,19 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  //validation handling
+  if (!email || !password) {
+    throw Error(
+      "No empty fields allowed, please fill in both password and email"
+    );
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Please enter a valid Email address");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Please enter a stronger password");
+  }
+
   const exists = await this.findOne({ email });
   if (exists) throw Error("This email address has already been used");
 
