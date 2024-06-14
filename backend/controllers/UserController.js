58bb0345f9f58a16f64a8fe2b4635fb8b9f77a03
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-06-14 15:23:00 +0530

    used bcrypt for hashing password for signup
    in the user model a static method was added to handle the signup. Not only would this throw an error if an already existing email is used, but it will also properly hash the password with salt and then save it to the database. this static method will be used in the userController's signup method, within a try catch so that any errors can be caught. BTW, mongoose will already give an error when an existing email is going to be added because the email field was defined as unique in the schema, but in using this signup method we can control what type of error message will be transmitted as a response when the user enters an already existing password

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,7 +4,13 @@ const login = async (req, res) => {
 };
 
 const signup = async (req, res) => {
-  res.status(200).json({ mssg: "signup request handled" });
+  const { email, password } = req.body;
+  try {
+    const createdUser = await User.signup(email, password);
+    res.status(200).json({ email, createdUser });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = { login, signup };
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const { schema } = require("./workoutModel");
 
@@ -14,5 +15,16 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) throw Error("This email address has already been used");
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const createdUser = this.create({ email, password: hash });
+  return createdUser;
+};
 
 module.exports = mongoose.model("user", userSchema);
